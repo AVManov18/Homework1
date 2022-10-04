65236c36bb3fbecb671530ca0ef617462d78b50a
AVManov18 <58166548+AVManov18@users.noreply.github.com>
2022-10-04 15:39:23 +0300

    Add files via upload

---
+++ src/Dog.java
@@ -0,0 +1,31 @@
+public class Dog {
+
+    private String gender;
+    private int year;
+    private String name;
+
+
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getGender() {
+        return gender;
+    }
+
+
+    public void setAge(int year) {
+        this.year = year;
+    }
+    public int getAge() {
+        return year;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+}
---
+++ src/MainDog.java
@@ -0,0 +1,20 @@
+public class MainDog {
+
+
+    public static void main(String args[]) {
+        Dog kuche = new Dog();
+
+        kuche.setGender("Male");
+        kuche.setAge(5);
+        kuche.setName("Bademcho");
+
+        System.out.println(kuche.getGender());
+        System.out.println(kuche.getAge());
+        System.out.println(kuche.getName());
+
+
+
+    }
+
+
+}
